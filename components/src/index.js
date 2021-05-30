3f5cd913a42c90bf48f63dda55b4d1ffcf0113ab
pkimln8 <pkimln8@gmail.com>
2021-05-29 23:40:16 -0400

    Component Nesting

---
+++ components/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
