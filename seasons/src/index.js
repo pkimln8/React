a708bdb7c733ef045fca667da26c508ed26e04ee
pkimln8 <pkimln8@gmail.com>
2021-06-04 17:15:14 -0400

    Lifecycle Methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
+  // Same as:
+  // constructor(props) {
+  //   super(props);
 
-    // This is the ONLY time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' };
+  //   // This is the ONLY time we do direct assignment to this.state
+  //   this.state = { lat: null, errorMessage: '' };
+  // }
 
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => this.setState({ errorMessage: err.message })
-    );
-  }
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
 
-  componentDidMount() {
-    console.log('My component was rendered to the screen');
-  }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
 
-  componentDidUpdate() {
-    console.log('My component was just updated - it rendered!');
+    return <Spinner message="Please accept location request" />;
   }
 
   render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
-    return <div>Loading...</div>;
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
 }
 
