7a50a8ad68721888d0983dbc1694ccc78d7ff4e8
pkimln8 <pkimln8@gmail.com>
2021-06-18 00:06:54 -0400

    axios setup

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBg9-rs1gdIxQWNtrxh66WesoLZb3hoSQc';
+
+export default axios.create({
+  baseURL: 'GET https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: '5',
+    type: 'video',
+    key: KEY,
+  }
+});
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
     event.preventDefault();
 
     // Todo: Make sure we call callback from parent component
+    this.props.onFormSubmit(this.state.term);
   }
 
   render() {
